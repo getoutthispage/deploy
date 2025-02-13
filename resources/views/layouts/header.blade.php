e6b0a42920963b86695c03cee32625a50ba7b681
getoutthispage <getoutthispage@gmail.com>
2025-02-13 16:10:42 +0500

    Initial commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>indigoshop</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+
+</head>
+<body>
+<header>
+
+</header>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\IndexController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [IndexController::class, 'index'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
